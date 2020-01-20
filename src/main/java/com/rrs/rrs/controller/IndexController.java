d558ade13b0b96ccfd14cb984558f96af784dadb
lgy <lgy-lgy-l@qq.com>
2020-01-20 21:25:17 +0800

    首页

--- src/main/java/com/rrs/rrs/controller/IndexController.java
+++ src/main/java/com/rrs/rrs/controller/IndexController.java
@@ -1,6 +1,5 @@
-package com.bms.bms.controller;
+package com.rrs.rrs.controller;
 
-import com.bms.bms.exception.CustomizeErrorCode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     @GetMapping("/")
-    public String index(){
-
-
+    public String index(Model model){
+        model.addAttribute("backgroundPic", "/images/background/restaurant4.jpg");
+        model.addAttribute("slogan", "温暖你的是服务，感动你的是美食");
         return "index";
     }
 
     @GetMapping("/noLogin")
     public String noLogin(Model model){
 
-        model.addAttribute("errorMessage", CustomizeErrorCode.NO_LOGIN.getMessage());
-        model.addAttribute("errorCode", CustomizeErrorCode.NO_LOGIN.getCode());
+        model.addAttribute("backgroundPic", "/images/restaurant.jpg");
+        model.addAttribute("errorCode", null);
         return "error";
     }
 
