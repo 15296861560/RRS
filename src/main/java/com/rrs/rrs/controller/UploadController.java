e15331e98b5c2943f059a5aafdb4f24cbe4f4d4d
lgy <lgy-lgy-l@qq.com>
2020-02-08 18:29:34 +0800

    修复显示图片

--- src/main/java/com/rrs/rrs/controller/UploadController.java
+++ src/main/java/com/rrs/rrs/controller/UploadController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 
 @Controller
@@ -81,37 +80,5 @@ public class UploadController {
 
 
     }
-
-//    public static boolean uploadfile(MultipartFile file, String path, String fileName){
-//
-//        // 生成新的文件名
-//        //String realPath = path + "/" + FileNameUtils.getFileName(fileName);
-//
-//        //使用原文件名
-//        String realPath = path + "/" + fileName;
-//
-//        File dest = new File(realPath);
-//
-//        //判断文件父目录是否存在
-//        if(!dest.getParentFile().exists()){
-//            dest.getParentFile().mkdir();
-//        }
-//
-//        try {
-//            //保存文件
-//            file.transferTo(dest);
-//            return true;
-//        } catch (IllegalStateException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//            return false;
-//        } catch (IOException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//            return false;
-//        }
-//
-//    }
-
 
 }
