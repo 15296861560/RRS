14be5437aa270dbefa86b2e58e3353bbbf43a5dc
lgy <lgy-lgy@qq.com>
2020-02-10 01:50:24 +0800

    添加食物类型

--- src/main/java/com/rrs/rrs/enums/FoodTypeEnum.java
+++ src/main/java/com/rrs/rrs/enums/FoodTypeEnum.java
@@ -1,8 +1,15 @@
 package com.rrs.rrs.enums;
 
 public enum FoodTypeEnum {
-    A("A","炒菜"),
-    D("D","饮料");
+    A("A","小炒"),
+    B("B","蒸菜"),
+    C("C","凉菜"),
+    D("D","甜点"),
+    E("E","汤类"),
+    F("F","主食"),
+    G("G","海鲜"),
+    H("H","美酒"),
+    I("I","饮料");
 
 
     private String type;
