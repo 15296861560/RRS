c9e2c03c0fd0a09cfaee6853a56ed60a4412986d
lgy <lgy-lgy@qq.com>
2020-02-24 16:48:11 +0800

    修改结算成功后的提示效果

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -16,17 +16,18 @@ function settle(e) {
         }),
         success: function (response) {
             if (response.code == 200) {//预约成功
-                window.location.href="/profile/basket";
                 Swal.fire({
                     icon: 'success',
                     title: '结算成功！',
                     text: '祝您用餐愉快☺',
                 });
+                // 3秒后自动刷新页面
+                var t = setTimeout(function(){window.location.reload();},3000);
             } else {
                 Swal.fire({
                     icon: 'error',
                     title: '结算失败！',
-                    text: '请重新尝试！',
+                    text: '请检查是否已经预约过位置！',
                 });
             }
         }
